3a5b0a67a6bd5979fc9441f401ff473f61571bc2
Harold <haroldtpozo@gmail.com>
2024-02-24 11:07:08 +0100

    Primer commit con creacion de rutas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import React from 'react'
+import Menu from "../src/components/menu/Menu"
+import Orders from "../src/components/orders/Orders"
+import Layaout from './components/layout/Layaout'
+import { Route,Routes } from 'react-router-dom'
 
-function App() {
-    return (
-    <div>
-        Your App here
-    </div>
-
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layaout/>}></Route>
+      <Route path="/menu" element={<Menu/>}></Route>
+      <Route path="/orders" element={<Orders/>}></Route>
+    </Routes>
 
   )
 }
-
-export default App
