54736a55cfd261bb47575b8c5eca90f700c19687
Artbat03 <artbat03@gmail.com>
2024-02-24 13:03:10 +0100

    feat (MENU-RES) Created menu bot response

--- backend/src/bot/index.ts
+++ backend/src/bot/index.ts
@@ -1,21 +1,38 @@
-import {Context, Telegraf} from "telegraf";
-import {ChatUser} from "@hackathon/types";
+import { Context, Telegraf } from 'telegraf';
+import { ChatUser, Menu, Plate } from '@hackathon/types';
 
+import { MenuEntity } from '../entities/menu';
 
 export interface MyContext extends Context {
-    chatUser?: ChatUser
+  chatUser?: ChatUser;
 }
 
-const configureBot =() =>  {
-    const bot = new Telegraf<MyContext>(process.env.BOT_TOKEN);
+const plateToString = (plate: Plate) => {
+  return `${plate.name}: *${plate.description}* `;
+};
 
-    bot.start(async (ctx) => {
-        await ctx.replyWithMarkdown('Welcome to MenuBot');
-    });
+const menuToString = (menu: Menu) => {
+  return `*${menu.name}:* ${menu.price}€ \n - ${menu.plates
+    .map(plateToString)
+    .join('\n')}`;
+};
 
+const configureBot = () => {
+  const bot = new Telegraf<MyContext>(process.env.BOT_TOKEN);
 
-    return bot;
-}
+  bot.start(async (ctx) => {
+    await ctx.replyWithMarkdown('Welcome to Oido Cocina Bot');
+  });
 
+  bot.command('menu', async (ctx) => {
+    const MenuData = await MenuEntity.find();
+
+    ctx.replyWithMarkdown(
+      `Here are the available menus: \n${MenuData.map(menuToString).join('\n')}`
+    );
+  });
+
+  return bot;
+};
 
 export default configureBot;
